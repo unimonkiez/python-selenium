35db63f72ef489281f3b6d2f95fff22f3e30c15f
Yuval Saraf <unimonkiez@gmail.com>
2023-05-28 01:28:49 +0300

    Working tests in docker

--- start.py
+++ start.py
@@ -11,9 +11,7 @@ def set_chrome_options() -> Options:
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_prefs = {}
-    chrome_options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    chrome_options.add_argument('ignore-certificate-errors')
     return chrome_options
 
 def start() -> None:
